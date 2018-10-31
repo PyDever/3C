b3fa7675b163ac78e46d659edabfc7ea5c4d12ca
Kaleb Horvath <bobafett2021@hotmail.com>
2018-10-30 21:27:16 -1000

    added aircat_waps and aircat_clients

---
+++ aircat_clients/__main__.py
@@ -0,0 +1,52 @@
+
+import sys
+import datetime
+import optparse
+from scapy.all import *
+
+# function to parse command line args
+def get_arguments():
+	parser = optparse.OptionParser()
+	parser.add_option("-t", "--target", dest="target", 
+		help="Target IP / IP range.")
+	options, arguments = parser.parse_args()
+	return options
+
+# carefully craft ARP ping packet with datalink access
+options = get_arguments()
+ip_address = options.target
+pkt = ARP(pdst=ip_address)
+broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
+
+try:
+	# send the ARP ping packt crafted above
+	print("\n[*] Broadcasting packet...")
+	answers = srp(broadcast/pkt, timeout=1, verbose=False)[0]
+
+except KeyboardInterrupt as interrupt:
+	print("[!] Quitting...")
+	sys.exit(0) # quit and raise no error
+
+# collect list of clients from answers
+counter = -1
+print("[*] List of clients obtained: ")
+for element in answers:
+	counter += 1
+
+	# attempt a reverse DNS lookup
+	client_name = ''
+	try:
+		client_name = socket.gethostbyaddr(element[1].psrc)[0]
+
+	except (socket.error, Exception) as error:
+		print("[!] Reverse DNS lookup FAILED!")
+		print("[!] Displaying results anyways...")
+		sys.exit(1) # exit and raise system error code
+
+	print("%i) %s - %s - %s" % (
+		counter, element[1].psrc, element[1].hwsrc,
+		client_name))
+
+
+
+
